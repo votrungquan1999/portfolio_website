a5bebf0b07c89b06a3a2bded848c724cc32420dc
Quan Vo <votrungquan99@gmail.com>
2019-05-23 22:35:11 +0700

    Updates

---
+++ precache-manifest.0c1d4b018b48ca55e7fa0dbb891e44c4.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "5127f2fd519a2bb62a3dd321dd26bf5f",
+    "url": "/porfolio_website/static/media/tryp-home.5127f2fd.png"
+  },
+  {
+    "revision": "c4b4c4cb03117ff7cdc880eea9db0789",
+    "url": "/porfolio_website/static/media/resume-quan-vo-official.c4b4c4cb.docx"
+  },
+  {
+    "revision": "34294cc3e66f4ab870ab8e5f88713c9f",
+    "url": "/porfolio_website/static/media/profile.34294cc3.png"
+  },
+  {
+    "revision": "e7e7662586cc408038791c1a3dff02bd",
+    "url": "/porfolio_website/static/media/home_background2.e7e76625.svg"
+  },
+  {
+    "revision": "8f256605e2c8b2140f5e",
+    "url": "/porfolio_website/static/js/runtime~main.50c7c939.js"
+  },
+  {
+    "revision": "3d682b9388853183ff02",
+    "url": "/porfolio_website/static/js/main.25d54eca.chunk.js"
+  },
+  {
+    "revision": "17f148b06515ea2b88cd",
+    "url": "/porfolio_website/static/js/2.09deb9ea.chunk.js"
+  },
+  {
+    "revision": "3d682b9388853183ff02",
+    "url": "/porfolio_website/static/css/main.4c7d532f.chunk.css"
+  },
+  {
+    "revision": "d0bc23251825c3115a7b5827b8131a95",
+    "url": "/porfolio_website/index.html"
+  }
+];
